9ad95e6a3769c3256cf151af6c55d6081f1c9a42
quangle0309 <quang9999xx.1@gmail.com>
2024-04-07 21:15:27 +0700

    add new folder

---
+++ module1/ss6_tong_quan_javascript/demo_thuyet_trinh/demo.js
@@ -0,0 +1,9 @@
+
+// Đây là một chú thích dòng đơn.
+var x = 5; // Gán giá trị 5 cho biến x
+
+/*
+Đây là một chú thích nhiều dòng
+Có thể chứa nhiều dòng mã và thông tin mô tả
+*/
+var y = 10; // Gán giá trị 10 cho biến y
