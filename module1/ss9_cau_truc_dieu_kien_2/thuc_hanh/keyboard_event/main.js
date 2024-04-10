db00687616dbabb57f9842859bbb1325e08bb369
quangle0309 <quang9999xx.1@gmail.com>
2024-04-10 14:05:09 +0700

    add new folder

---
+++ module1/ss9_cau_truc_dieu_kien_2/thuc_hanh/keyboard_event/main.js
@@ -0,0 +1,39 @@
+function upArrow() {
+    let element = document.getElementById("nobita");
+    element.style.top = parseInt(element.style.top) - 5 + "px";
+}
+
+function downArrow() {
+    let element = document.getElementById("nobita");
+    element.style.top = parseInt(element.style.top) + 5 + "px";
+}
+
+function leftArrow() {
+    let element = document.getElementById("nobita");
+    element.style.left = parseInt(element.style.left) - 5 + "px";
+}
+
+function rightArrow() {
+    let element = document.getElementById("nobita");
+    element.style.left = parseInt(element.style.left) + 5 + "px";
+}
+
+function moveSelection (evt) {
+    switch (evt.keyCode) {
+        case 37:
+            leftArrow();
+            break;
+        case 38:
+            upArrow();
+            break;
+        case 39:
+            rightArrow();
+            break;
+        case 40:
+            downArrow();
+            break;
+    }
+}
+function docReady() {
+    window.addEventListener("keydown", moveSelection);
+}
